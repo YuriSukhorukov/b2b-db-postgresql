12fe3b7542aeb135ddb65b9f8044acee556ca5ca
yurisukhorukov <yuri.sukhorukov.spb@gmail.com>
2020-11-06 03:06:40 +0300

    src: volumes in other script

--- docker/container.sh
+++ docker/container.sh
@@ -1,8 +1,5 @@
 docker container rm b2b-db-demo -f
 
-docker volume rm postgres_vol_1
-docker volume create postgres_vol_1
-
 docker run \
 -e POSTGRES_DB=b2b \
 -e POSTGRES_USER=postgres \
---
+++ docker/volumes.sh
@@ -0,0 +1,2 @@
+docker volume rm postgres_vol_1
+docker volume create postgres_vol_1
